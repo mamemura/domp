c05be600143ea45f01d1ff91a7745894dbfc44c4
nakamura-makoto <nakamura-makoto@dmm.com>
2016-12-07 18:31:19 +0900

    fix defalt message

--- command.go
+++ command.go
@@ -10,12 +10,12 @@ var GlobalFlags = []cli.Flag{
 	cli.StringFlag{
 		Name: "query,q",
 		Value: "text",
-		Usage: "query. can be used html/text/outerhtml/attrs/attr@xxx, seperator \"|\". e.g. --query \"html|attr@id\" , default \"text\".",
+		Usage: "query. can be used html/text/outerhtml/attrs/attr@xxx, seperator \"|\". e.g. --query \"html|attr@id\".",
 	},
 	cli.StringFlag{
 		Name: "output,o",
 		Value: "text",
-		Usage: "output. can be used text/csv/json, e.g. --query \"csv\"  default text.",
+		Usage: "output. can be used text/csv/json, e.g. --query \"csv\".",
 	},
 }
 
